cce3c7d636f92065607bf0bacbaee6a4170d6d56
Nigel Babu <nigelbabu@gmail.com>
2013-12-30 16:45:53 +0530

    Show output

--- ckanext/apihelper/resource/scripts/apihelper.js
+++ ckanext/apihelper/resource/scripts/apihelper.js
@@ -16,3 +16,25 @@ ckan.module('apihelper', function (jQuery, _) {
     },
   };
 });
+ckan.module('apihelper-submit', function (jQuery, _) {
+  return {
+    initialize: function () {
+      this.el.click(this._click);
+    },
+    _click: function (e) {
+      e.preventDefault();
+      var action = $('#field-actions option:selected')[0].value;
+      var output = '';
+      if(action_help[action] === undefined) {
+        return
+      }
+      var r = $.get(ckan.SITE_ROOT + '/api/3/action/' + action, function(data) {
+        var output_area = $('#apihelper-output');
+        output_area.text(JSON.stringify(data, null, "\t"));
+      });
+      r.fail(function(data) {
+        console.log('failed with %o', data);
+      });
+    },
+  };
+});
